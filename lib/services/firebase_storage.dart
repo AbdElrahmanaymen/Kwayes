714d7dd8d6fd332cfb55135f2d0f30ecb51d0186
Abd-Elrahman Aymen <30832681+AbdElrahmanaymen@users.noreply.github.com>
2021-11-03 11:59:32 +0200

    update

--- lib/services/firebase_storage.dart
+++
@@ -1,8 +0,0 @@
-import 'package:firebase_storage/firebase_storage.dart';
-
-Future<String> downloadUrl(String path) {
-  return FirebaseStorage.instance
-      .refFromURL('gs://kwayes-a3377.appspot.com/')
-      .child(path)
-      .getDownloadURL();
-}
